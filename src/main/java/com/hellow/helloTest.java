f7a6d936bf1a6904d1bdd3bc46d8cee3ea817625
fiercehuang <976984309@qq.com>
2019-07-09 15:05:40 +0800

    15：04

--- src/main/java/com/hellow/helloTest.java
+++ src/main/java/com/hellow/helloTest.java
@@ -3,5 +3,6 @@ package com.hellow;
 public class helloTest {
     public static void main(String[] args) {
         System.out.println("lalallaajsdksjad");
+        System.out.println("askjhdjgasjdj");
     }
 }
