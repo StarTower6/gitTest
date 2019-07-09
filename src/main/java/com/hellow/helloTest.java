7c10a088ee73c4e62a0adbe39c084a4924e6dfc1
StarTower6 <1352913877@qq.com>
2019-07-09 16:03:03 +0800

    测试Git2

--- src/main/java/com/hellow/helloTest.java
+++ src/main/java/com/hellow/helloTest.java
@@ -2,8 +2,24 @@ package com.hellow;
 
 public class helloTest {
     public static void main(String[] args) {
-        System.out.println("lalallaajsdksjad");
-        System.out.println("askjhdjgasjdj");
-        System.out.println("askjhdjgasjdj");
+        int[] array = {1,15,-1,0,2,1,1,0,1,1000};
+        int max = 0;
+        int sum = 0;
+        int sumMax = 0;
+        for (int i=0;i<array.length;i++){
+            sum = 0;
+           for (int j=i;j<array.length;j++){
+               if (array[i]==array[j]){
+                   sum++;
+               }
+           }
+           if(sum>sumMax){
+               sumMax=sum;
+               max = array[i];
+
+           }
+        }
+        System.out.println(sumMax);
+        System.out.println(max);
     }
 }
